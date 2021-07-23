949b65a7be4466455a41e6c0c85a4bea2e0773ef
Aidah Al Baqir <aidah.albaqir@gmail.com>
2021-07-23 10:03:48 -0500

    commit tree 9dd791ca8cc030e08651caf0bcad0b7e4b0f0864

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+let customerName = "Bob" 
+console.log(customerName)
+
+//global scope
+function upperCaseCustomerName () {
+    return upperCaseCustomerName ('${customerName}'.toUpperCase) 
+}
+const setBestCustomer = () =>{
+
+}
+let changeLeastFavoriteCustomer = you 
+let overwriteBestCustomer = (setBestCustomer) => {
+    return setBestCustomer
+let changeLeastFavoriteCustomer = grandpappy;
+}
